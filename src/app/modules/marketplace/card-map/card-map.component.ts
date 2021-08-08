bf5faaf14130b84c69c0178bc73de1561995bce6
awais786327 <awais3227443@gmail.com>
2021-08-08 17:52:00 +0500

    card grid, card list, card map marketplace module components

---
+++ src/app/modules/marketplace/card-map/card-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-map',
+  templateUrl: './card-map.component.html',
+  styleUrls: ['./card-map.component.scss']
+})
+export class CardMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/modules/marketplace/shared/marketplace-side-menu/marketplace-side-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-marketplace-side-menu',
+  templateUrl: './marketplace-side-menu.component.html',
+  styleUrls: ['./marketplace-side-menu.component.scss']
+})
+export class MarketplaceSideMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
