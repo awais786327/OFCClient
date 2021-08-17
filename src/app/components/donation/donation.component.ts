167a34f54850c906a19fa3cf091fb4fe6a2d1236
awais786327 <awais3227443@gmail.com>
2021-08-18 03:26:33 +0500

    update donation

--- src/app/components/donation/donation.component.ts
+++ src/app/components/donation/donation.component.ts
@@ -31,14 +31,42 @@ export class DonationComponent implements OnInit {
       $('.' + className + '1').addClass('active');
     });
 
-    $('.modal-header .btn-close').click(function () {
-      $('.modal, .modal-backdrop').removeClass('show');
-      $('.modal, .modal-backdrop').addClass('hide');
-
-      setTimeout(() => {
-        $('.modal, .modal-backdrop').removeClass('d-block');
-        $('.modal, .modal-backdrop').addClass('d-none');
-      }, 500);
+    $('.modal-header .btn-close').click(() => {
+      this.hideModal();
     });
+  }
+
+  hideModal(){
+    $('.modal, .modal-backdrop').removeClass('show');
+    $('.modal, .modal-backdrop').addClass('hide');
+
+    setTimeout(() => {
+      $('.modal, .modal-backdrop').removeClass('d-block');
+      $('.modal, .modal-backdrop').addClass('d-none');
+    }, 500);
+  }
+
+  openCheckoutModal(){
+    this.hideModal();
+
+    setTimeout(() => {
+      $("#checkout_popup, .modal-backdrop").removeClass("hide d-none");
+      // $('.modal-backdrop').removeClass('hide d-none');
+
+      $("#checkout_popup, .modal-backdrop").addClass("show d-block");
+      // $('.modal-backdrop').addClass('show d-block');
+    }, 500);
+  }
+
+  openAttachmentModal(){
+    this.hideModal();
+
+    setTimeout(() => {
+      $("#upload, .modal-backdrop").removeClass("hide d-none");
+      // $('.modal-backdrop').removeClass('hide d-none');
+
+      $("#upload, .modal-backdrop").addClass("show d-block");
+      // $('.modal-backdrop').addClass('show d-block');
+    }, 500);
   }
 }
